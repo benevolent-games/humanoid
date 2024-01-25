1b2144d3cde3a85028ea2397b94af54150e2f983
Chase Moskal <chasemoskal@gmail.com>
2024-01-25 15:52:59 -0800

    ecs4: delete old ecs3 systems

--- s/ecs/hub.ts
+++ s/ecs/hub.ts
@@ -1,13 +1,7 @@
 
-import {Ecs3, Ecs4} from "@benev/toolbox"
+import {Ecs4} from "@benev/toolbox"
 import {Realm} from "../models/realm/realm.js"
 import {HumanoidSchema, HumanoidTick} from "./schema.js"
-
-export const hub = new Ecs3.Hub<
-	Realm,
-	HumanoidTick,
-	HumanoidSchema
->()
 
 export const hub2 = new Ecs4.Hub<Realm, HumanoidTick, HumanoidSchema>()
 export const {system, behavior} = hub2
