441aed7fc751c25c914cad89505f2493f51fe7a6
Chase Moskal <chasemoskal@gmail.com>
2024-02-17 22:45:04 -0800

    effects: wip rework

--- s/dom/views/panel/view.ts
+++ s/dom/views/panel/view.ts
@@ -1,101 +1,35 @@
 
-import {TemplateResult, clone, debounce, html, reactor} from "@benev/slate"
+import {TemplateResult, clone, debounce, html, reactor, ob, flat} from "@benev/slate"
 
 import {styles} from "./styles.js"
 import {nexus} from "../../../nexus.js"
 import {Toggler} from "./parts/toggler.js"
 import {HumanoidRealm} from "../../../models/realm/realm.js"
-import {Effects, NuiCheckbox, NuiRange, Stage} from "@benev/toolbox"
+import {Effects, NuiCheckbox, NuiRange, Rendering} from "@benev/toolbox"
 
 export const Panel = nexus.shadow_view(use => (realm: HumanoidRealm) => {
 	use.name("panel")
 	use.styles(styles)
 
 	const resolution = use.signal(realm.porthole.resolution * 100)
-	const std = use.once(() => Stage.effects.everything())
-
-	const ssao = use.flatstate(std.ssao)
-	const ssr = use.flatstate(std.ssr)
-
-	const antialiasing = use.flatstate(std.default.antialiasing)
-	const imageProcessing = use.flatstate(std.default.imageProcessing)
-	const bloom = use.flatstate(std.default.bloom)
-	const grain = use.flatstate(std.default.grain)
-	const sharpen = use.flatstate(std.default.sharpen)
-	const chromaticAberration = use.flatstate(std.default.chromaticAberration)
-	const glow = use.flatstate(std.default.glow)
-	const depthOfField = use.flatstate(std.default.depthOfField)
-
-	const active = use.flatstate({
-		ssao: false,
-		ssr: false,
-
-		default: false,
-		antialiasing: false,
-		imageProcessing: false,
-		bloom: false,
-		grain: false,
-		sharpen: false,
-		chromaticAberration: false,
-		glow: false,
-		depthOfField: false,
+	const {effects, active} = use.once(() => {
+		const std = Rendering.effects.everything()
+		const effects = ob(std).map(effect => flat.state(effect)) as Effects
+		const active = flat.state(ob(std).map(() => false))
+		return {effects, active}
 	})
 
 	function collect() {
-		const effects = clone<Effects>({
-			ssao,
-			ssr,
-			default: {
-				antialiasing,
-				imageProcessing,
-				bloom,
-				grain,
-				sharpen,
-				chromaticAberration,
-				glow,
-				depthOfField,
-			},
-		})
-
-		if (!active.ssao)
-			effects.ssao = null
-
-		if (!active.ssr)
-			effects.ssr = null
-
-		if (!active.antialiasing)
-			effects.default!.antialiasing = null
-
-		if (!active.imageProcessing)
-			effects.default!.imageProcessing = null
-
-		if (!active.bloom)
-			effects.default!.bloom = null
-
-		if (!active.grain)
-			effects.default!.grain = null
-
-		if (!active.sharpen)
-			effects.default!.sharpen = null
-
-		if (!active.chromaticAberration)
-			effects.default!.chromaticAberration = null
-
-		if (!active.glow)
-			effects.default!.glow = null
-
-		if (!active.depthOfField)
-			effects.default!.depthOfField = null
-
-		if (!active.default)
-			effects.default = null
-
-		return effects
+		const copy = clone(effects) as Partial<Effects>
+		for (const [key, value] of Object.entries(active))
+			if (value === false)
+				copy[key as keyof typeof copy] = undefined
+		return copy
 	}
 
 	const json = use.signal(JSON.stringify(collect()))
 
-	const apply_the_effects = use.once(() => debounce(500, (effects: Effects) => {
+	const apply_the_effects = use.once(() => debounce(500, (effects: Partial<Effects>) => {
 		json.value = JSON.stringify(effects)
 		realm.stage.rendering.setEffects(effects)
 	}))
@@ -124,13 +58,12 @@ export const Panel = nexus.shadow_view(use => (realm: HumanoidRealm) => {
 		max: number
 		step: number
 	}
-	type Group = {[key: string]: number | boolean}
+	type Group = {[key: string]: number | boolean | string}
 	type NumSpec<G extends Group> = {
 		[K in keyof G as G[K] extends number ? K : never]: Granularity
 	}
 
 	const granularity = {
-		alphaMode: {min: 0, max: 5, step: 1},
 		samples: {min: 0, max: 64, step: 2},
 		bigSamples: {min: 0, max: 512, step: 8},
 		integer: {min: 0, max: 100, step: 1},
@@ -161,6 +94,9 @@ export const Panel = nexus.shadow_view(use => (realm: HumanoidRealm) => {
 						set: x => (group as any)[key] = x,
 					}])
 				}
+				else if (typeof value === "string") {
+					return html`<span>TODO dropdown</span>`
+				}
 				else throw new Error(`invalid setting "${key}"`)
 			})
 		}
@@ -188,37 +124,14 @@ export const Panel = nexus.shadow_view(use => (realm: HumanoidRealm) => {
 
 	function handle_json_change(event: InputEvent) {
 		const textarea = event.currentTarget as HTMLTextAreaElement
-		const effects = JSON.parse(textarea.value) as Effects
-
-		active.ssao = !!effects.ssao
-		active.ssr = !!effects.ssr
-
-		Object.assign(ssao, effects.ssao)
-		Object.assign(ssr, effects.ssr)
-
-		active.default = !!effects.default
-
-		if (effects.default) {
-			active.antialiasing = !!effects.default?.antialiasing
-			active.imageProcessing = !!effects.default?.imageProcessing
-			active.bloom = !!effects.default?.bloom
-			active.grain = !!effects.default?.grain
-			active.sharpen = !!effects.default?.sharpen
-			active.chromaticAberration = !!effects.default?.chromaticAberration
-			active.glow = !!effects.default?.glow
-			active.depthOfField = !!effects.default?.depthOfField
-
-			Object.assign(antialiasing, effects.default.antialiasing)
-			Object.assign(imageProcessing, effects.default.imageProcessing)
-			Object.assign(bloom, effects.default.bloom)
-			Object.assign(grain, effects.default.grain)
-			Object.assign(sharpen, effects.default.sharpen)
-			Object.assign(chromaticAberration, effects.default.chromaticAberration)
-			Object.assign(glow, effects.default.glow)
-			Object.assign(depthOfField, effects.default.depthOfField)
+		const newEffects = JSON.parse(textarea.value.trim()) as Partial<Effects>
+		for (const [k, group] of Object.entries(newEffects)) {
+			const key = k as keyof Effects
+			active[key] = !!group
+			if (group)
+				Object.assign(effects[key], group)
 		}
-
-		apply_the_effects(effects)
+		apply_the_effects(newEffects)
 	}
 
 	return wrap(html`
@@ -240,63 +153,7 @@ export const Panel = nexus.shadow_view(use => (realm: HumanoidRealm) => {
 		</article>
 
 		<article>
-			<header ?data-active="${active.default}">
-				${NuiCheckbox([{
-					label: "default",
-					checked: active.default,
-					set: x => active.default = x,
-				}])}
-				<a target=_blank href="https://doc.babylonjs.com/features/featuresDeepDive/postProcesses/defaultRenderingPipeline">docs</a>
-			</header>
-			<article ?data-hidden="${!active.default}">
-				${render_section("antialiasing", antialiasing)({
-					samples: granularity.samples,
-				})}
-
-				${render_section("imageProcessing", imageProcessing)({
-					contrast: granularity.medium,
-					exposure: granularity.medium,
-				})}
-
-				${render_section("bloom", bloom)({
-					weight: granularity.medium,
-					threshold: granularity.fine,
-					scale: granularity.medium,
-					kernel: granularity.bigSamples,
-				})}
-
-				${render_section("chromaticAberration", chromaticAberration)({
-					aberrationAmount: granularity.coarse,
-					radialIntensity: granularity.medium,
-					alphaMode: granularity.alphaMode,
-				})}
-
-				${render_section("glow", glow)({
-					intensity: granularity.medium,
-					blurKernelSize: granularity.samples,
-				})}
-
-				${render_section("grain", grain)({
-					intensity: granularity.coarse,
-				})}
-
-				${render_section("sharpen", sharpen)({
-					colorAmount: granularity.medium,
-					edgeAmount: granularity.medium,
-				})}
-
-				${render_section("depthOfField", depthOfField)({
-					blurLevel: granularity.medium,
-					fStop: granularity.medium,
-					focalLength: granularity.coarse,
-					focusDistance: granularity.giant,
-					lensSize: granularity.coarser,
-				})}
-			</article>
-		</article>
-
-		<article>
-			${render_section("ssao", ssao, html`
+			${render_section("ssao", effects.ssao, html`
 					<a target=_blank href="https://doc.babylonjs.com/typedoc/classes/BABYLON.SSAO2RenderingPipeline">ref</a>
 				`)({
 				ssaoRatio: granularity.medium,
@@ -312,10 +169,8 @@ export const Panel = nexus.shadow_view(use => (realm: HumanoidRealm) => {
 				epsilon: granularity.fine,
 				samples: granularity.samples,
 			})}
-		</article>
 
-		<article>
-			${render_section("ssr", ssr, html`
+			${render_section("ssr", effects.ssr, html`
 					<a target=_blank href="https://doc.babylonjs.com/features/featuresDeepDive/postProcesses/SSRRenderingPipeline">docs</a>
 					<a target=_blank href="https://doc.babylonjs.com/typedoc/classes/BABYLON.SSRRenderingPipeline">ref</a>
 				`)({
@@ -324,7 +179,7 @@ export const Panel = nexus.shadow_view(use => (realm: HumanoidRealm) => {
 				reflectionSpecularFalloffExponent: granularity.medium,
 				roughnessFactor: granularity.fine,
 				strength: granularity.medium,
-				blurDispersionStrength: granularity.medium,
+				blurDispersionStrength: granularity.fine,
 				reflectivityThreshold: granularity.superfine,
 				blurDownsample: granularity.medium,
 				ssrDownsample: granularity.medium,
@@ -333,6 +188,54 @@ export const Panel = nexus.shadow_view(use => (realm: HumanoidRealm) => {
 				thickness: granularity.medium,
 				selfCollisionNumSkip: granularity.medium,
 				backfaceDepthTextureDownsample: granularity.medium,
+			})}
+
+			${render_section("lens", effects.lens)({
+				chromatic_aberration: granularity.fine,
+				edge_blur: granularity.fine,
+				distortion: granularity.fine,
+				grain_amount: granularity.fine,
+				dof_focus_distance: granularity.coarse,
+				dof_aperture: granularity.medium,
+				dof_darken: granularity.medium,
+				dof_gain: granularity.medium,
+				dof_threshold: granularity.medium,
+			})}
+
+			${render_section("antialiasing", effects.antialiasing)({
+				samples: granularity.samples,
+			})}
+
+			${render_section("imageProcessing", effects.imageProcessing)({
+				contrast: granularity.medium,
+				exposure: granularity.medium,
+			})}
+
+			${render_section("tonemapping", effects.tonemapping)({})}
+
+			${render_section("vignette", effects.vignette)({
+			})}
+
+			${render_section("bloom", effects.bloom)({
+				weight: granularity.medium,
+				threshold: granularity.fine,
+				scale: granularity.medium,
+				kernel: granularity.bigSamples,
+			})}
+
+			${render_section("chromaticAberration", effects.chromaticAberration)({
+				aberrationAmount: granularity.coarse,
+				radialIntensity: granularity.medium,
+			})}
+
+			${render_section("glow", effects.glow)({
+				intensity: granularity.medium,
+				blurKernelSize: granularity.samples,
+			})}
+
+			${render_section("sharpen", effects.sharpen)({
+				colorAmount: granularity.medium,
+				edgeAmount: granularity.medium,
 			})}
 		</article>
 	`)
