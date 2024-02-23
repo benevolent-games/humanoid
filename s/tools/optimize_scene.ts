b52ba95d4061741f40b98fc2788e012854cbbb76
Chase Moskal <chasemoskal@gmail.com>
2024-02-23 13:34:32 -0800

    optimization: revert to compat to save ssr

--- s/tools/optimize_scene.ts
+++ s/tools/optimize_scene.ts
@@ -2,9 +2,9 @@
 import {Scene, ScenePerformancePriority} from "@babylonjs/core/scene.js"
 
 export function optimize_scene(scene: Scene) {
-	scene.performancePriority = ScenePerformancePriority.Intermediate
+	scene.performancePriority = ScenePerformancePriority.BackwardCompatible
 	scene.autoClear = false
-	scene.autoClearDepthAndStencil = false
+	// scene.autoClearDepthAndStencil = false
 	scene.skipFrustumClipping = true
 	scene.skipPointerMovePicking = true
 	scene.skipPointerDownPicking = true
