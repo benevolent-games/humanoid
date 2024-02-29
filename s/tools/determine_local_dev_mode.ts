77981f2a5928b0f85483ad5c7685945c7ccc9833
Chase Moskal <chasemoskal@gmail.com>
2024-02-29 13:03:45 -0800

    dev: consider local cloudflare tunnels as dev

--- s/tools/determine_local_dev_mode.ts
+++ s/tools/determine_local_dev_mode.ts
@@ -4,7 +4,9 @@ export function determine_local_dev_mode(url: string) {
 	const params = new URLSearchParams(search)
 	return (
 		host.startsWith("localhost") ||
-		host.startsWith("192")
+		host.startsWith("192") ||
+		host.includes("cloudflare") ||
+		params.has("dev")
 	) && !params.has("cloud")
 }
 
