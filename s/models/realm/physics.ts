fd5bd7238ecbf7bc676130d6e8a2bd8b12e9befe
Chase Moskal <chasemoskal@gmail.com>
2024-03-08 19:50:33 -0800

    integrate: new physics

--- s/models/realm/physics.ts
+++ s/models/realm/physics.ts
@@ -2,15 +2,14 @@
 import {Scene} from "@babylonjs/core/scene.js"
 import {Physics, debug_colors} from "@benev/toolbox"
 
-export const preparePhysics = ({hz, scene, colors}: {
-		hz: number
+export const preparePhysics = ({scene, hertz, colors}: {
 		scene: Scene
+		hertz: number
 		colors: ReturnType<typeof debug_colors>
 	}) => new Physics({
-	hz,
+	hertz,
 	scene,
 	colors,
 	gravity: [0, -9.81, 0],
-	contact_force_threshold: 0.1,
 })
 
