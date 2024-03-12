59bc9acfae8121ce55e78c8622018324818e08b4
Chase Moskal <chasemoskal@gmail.com>
2024-03-12 00:31:41 -0700

    fix: leftover lightsaber bug

--- s/ecs/schema/hybrids/character/character.ts
+++ s/ecs/schema/hybrids/character/character.ts
@@ -1,7 +1,7 @@
 
 import {MeshBuilder} from "@babylonjs/core/Meshes/meshBuilder.js"
 import {TransformNode} from "@babylonjs/core/Meshes/transformNode.js"
-import {HybridComponent, Rapier, Vec3, babylonian, label, vec3} from "@benev/toolbox"
+import {HybridComponent, Rapier, Trashcan, Vec3, babylonian, label, vec3} from "@benev/toolbox"
 
 import {HuRealm} from "../../../../models/realm/realm.js"
 import {establish_anim_coordination} from "./choreography/establish_anim_coordination.js"
@@ -27,7 +27,11 @@ export class Character extends HybridComponent<HuRealm, {
 		const {sword} = this.parts
 		const {physics, scene} = this.realm
 
-		const swordtip = new TransformNode(label("sword-tip"))
+		const trash = new Trashcan()
+
+		const swordtip = trash.bag(
+			new TransformNode(label("sword-tip"))
+		).dump(t => t.dispose())
 		swordtip.parent = sword
 		swordtip.position.set(0, -3.3, 0)
 		sword.visibility = 0.5
@@ -36,32 +40,41 @@ export class Character extends HybridComponent<HuRealm, {
 		const scale: Vec3 = [.1, 4, .1]
 		const [width, height, depth] = scale
 
-		const swordproxy = MeshBuilder.CreateBox(
-			label("swordproxy"),
-			{width, height, depth},
-			scene,
-		)
+		const swordproxy = trash.bag(
+			MeshBuilder.CreateBox(
+				label("swordproxy"),
+				{width, height, depth},
+				scene,
+			)
+		).dump(m => m.dispose())
 		swordproxy.material = physics.colors.red
 		swordproxy.position.y = -1.5
 		swordproxy.parent = sword
 		swordproxy.visibility = 0.5
 
-		const box = physics.prefabs.box_collider({
-			density: 0,
-			sensor: true,
-			material: physics.colors.green,
-			scale: vec3.multiplyBy(scale, s),
-			position: babylonian.to.vec3(swordproxy.absolutePosition),
-			rotation: babylonian.ascertain.absoluteQuat(swordproxy),
-			contact_force_threshold: 0.02,
-			groups: physics.grouper.specify({
-				filter: [physics.grouper.all],
-				membership: [physics.groups.sensor],
-			}),
-		})
+		const box = trash.bag(
+			physics.prefabs.box_collider({
+				density: 0,
+				sensor: true,
+				material: physics.colors.green,
+				scale: vec3.multiplyBy(scale, s),
+				position: babylonian.to.vec3(swordproxy.absolutePosition),
+				rotation: babylonian.ascertain.absoluteQuat(swordproxy),
+				contact_force_threshold: 0.02,
+				groups: physics.grouper.specify({
+					filter: [physics.grouper.all],
+					membership: [physics.groups.sensor],
+				}),
+			})
+		).dump(b => b.dispose())
 		box.collider.setActiveEvents(Rapier.ActiveEvents.COLLISION_EVENTS)
 
-		return {box, swordtip, swordproxy}
+		return {
+			box,
+			swordtip,
+			swordproxy,
+			dispose: trash.dispose,
+		}
 	})()
 
 	created() {}
@@ -69,6 +82,7 @@ export class Character extends HybridComponent<HuRealm, {
 	deleted() {
 		this.parts.dispose()
 		this.coordination.dispose()
+		this.helpers.dispose()
 	}
 }
 
