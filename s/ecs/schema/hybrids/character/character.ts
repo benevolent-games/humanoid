193eba78f2ae5c7d49827d57c0e75d3892f08593
Chase Moskal <chasemoskal@gmail.com>
2024-04-04 03:23:18 -0700

    hide: extra weapons

--- s/ecs/schema/hybrids/character/character.ts
+++ s/ecs/schema/hybrids/character/character.ts
@@ -1,10 +1,8 @@
 
-import {MeshBuilder} from "@babylonjs/core/Meshes/meshBuilder.js"
+import {Trashcan, label} from "@benev/toolbox"
 import {TransformNode} from "@babylonjs/core/Meshes/transformNode.js"
-import {Rapier, Trashcan, Vec3, babylonian, label, vec3} from "@benev/toolbox"
 
 import {HybridComponent} from "../../../hub.js"
-import {HuRealm} from "../../../../models/realm/realm.js"
 import {establish_anim_coordination} from "./choreography/establish_anim_coordination.js"
 import {prepare_choreographer_babylon_parts} from "./choreography/prepare_choreographer_babylon_parts.js"
 
@@ -35,73 +33,13 @@ export class Character extends HybridComponent<{height: number}> {
 		swordtip.parent = sword
 		swordtip.position.set(0, 0, swordlength)
 
-		// const swordtip_visualizer = trash.bag(
-		// 	MeshBuilder.CreateIcoSphere(
-		// 		label("swordtipvis"),
-		// 		{radius: 0.05, subdivisions: 1},
-		// 		scene,
-		// 	)
-		// ).dump(m => m.dispose())
-		// swordtip_visualizer.material = colors.magenta
-		// swordtip_visualizer.parent = swordtip
-
 		const swordbase = trash.bag(
 			new TransformNode(label("swordbase"), scene)
 		).dump(t => t.dispose())
 		swordbase.parent = sword
 		swordbase.position.set(0, 0, 0)
 
-		// const swordbase_visualizer = trash.bag(
-		// 	MeshBuilder.CreateIcoSphere(
-		// 		label("swordbasevis"),
-		// 		{radius: 0.05, subdivisions: 1},
-		// 		scene,
-		// 	)
-		// ).dump(m => m.dispose())
-		// swordbase_visualizer.material = colors.green
-		// swordbase_visualizer.parent = swordbase
-
 		return {swordbase, swordtip, dispose: trash.dispose}
-
-		// const s = .36805
-		// const scale: Vec3 = [.1, 4, .1]
-		// const [width, height, depth] = scale
-
-		// const swordproxy = trash.bag(
-		// 	MeshBuilder.CreateBox(
-		// 		label("swordproxy"),
-		// 		{width, height, depth},
-		// 		scene,
-		// 	)
-		// ).dump(m => m.dispose())
-		// swordproxy.material = physics.colors.red
-		// swordproxy.position.y = -1.5
-		// swordproxy.parent = sword
-		// swordproxy.visibility = 0.5
-
-		// const box = trash.bag(
-		// 	physics.prefabs.box_collider({
-		// 		density: 0,
-		// 		sensor: true,
-		// 		material: physics.colors.green,
-		// 		scale: vec3.multiplyBy(scale, s),
-		// 		position: babylonian.to.vec3(swordproxy.absolutePosition),
-		// 		rotation: babylonian.ascertain.absoluteQuat(swordproxy),
-		// 		contact_force_threshold: 0.02,
-		// 		groups: physics.grouper.specify({
-		// 			filter: [physics.grouper.all],
-		// 			membership: [physics.groups.sensor],
-		// 		}),
-		// 	})
-		// ).dump(b => b.dispose())
-		// box.collider.setActiveEvents(Rapier.ActiveEvents.COLLISION_EVENTS)
-
-		// return {
-		// 	box,
-		// 	swordtip,
-		// 	swordproxy,
-		// 	dispose: trash.dispose,
-		// }
 	})()
 
 	created() {}
