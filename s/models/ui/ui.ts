5ea7ea7368e30b99b5ee517e389ee54c7e3c1500
Chase Moskal <chasemoskal@gmail.com>
2024-04-10 22:15:41 -0700

    sketch: new ui data

--- s/models/ui/ui.ts
+++ s/models/ui/ui.ts
@@ -2,6 +2,7 @@
 import {Ecs} from "@benev/toolbox"
 import {flatstate} from "@benev/slate"
 import {Sensitivity} from "./types.js"
+import {Weapon} from "../armory/weapon.js"
 import {MeleeAction, MeleeAim} from "../../ecs/components/topics/warrior.js"
 
 export class Ui {
@@ -25,6 +26,33 @@ export class Ui {
 			meleeAim: null | Ecs.ComponentState<MeleeAim>
 			meleeAction: null | Ecs.ComponentState<MeleeAction>
 		}
+	})
+
+	health = flatstate({
+		displayActive: false,
+		hp: 0,
+		bleed: 0,
+		bleedRate: 0,
+		stamina: 0,
+	})
+
+	equipment = flatstate({
+		displayActivated: false,
+		shield: false,
+		grips: {
+			fists: {
+				available: false,
+				active: false,
+			},
+			twohander: {
+				available: false,
+				active: false,
+			},
+			onehander: {
+				available: false,
+				active: false,
+			},
+		} satisfies Record<Weapon.Grip, any>
 	})
 }
 
