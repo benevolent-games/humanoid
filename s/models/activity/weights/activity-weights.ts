12418637df149fa74088bb4c78b607d3c35825c2
Chase Moskal <chasemoskal@gmail.com>
2024-04-18 22:08:22 -0700

    fix: zero weights import

--- s/models/activity/weights/activity-weights.ts
+++ s/models/activity/weights/activity-weights.ts
@@ -6,7 +6,7 @@ import {parryWeights} from "./kinds/parry-weights.js"
 import {ParryReport} from "../reports/parry.js"
 import {meleeWeights} from "./kinds/melee-weights.js"
 import {meleeReport} from "../reports/melee/melee-report.js"
-import {zeroWeights} from "../../choreographer/activities/kit/zero-weights.js"
+import {zeroWeights} from "./utils/zero-weights.js"
 
 export function activityWeights(activity: Activity.Any | null) {
 	if (!activity)
