ee63bd4825d85f45a3e292916a3f0cc4c8122188
Chase Moskal <chasemoskal@gmail.com>
2024-04-20 19:52:03 -0700

    reorganize: angles.ts into reports dir

--- s/models/activity/exports.ts
+++ s/models/activity/exports.ts
@@ -1,5 +1,5 @@
 
-export * as Angles from "./melee/angles.js"
 export * as Activity from "./types/activity.js"
 export * as Maneuver from "./types/maneuver.js"
+export * as Angles from "./reports/melee/angles.js"
 
