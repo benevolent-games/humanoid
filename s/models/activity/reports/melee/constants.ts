d557ff4832cc16af084c776e903c11b3934c2f09
Chase Moskal <chasemoskal@gmail.com>
2024-04-24 15:24:00 -0700

    tweak: comboableReleaseThreshold 0

--- s/models/activity/reports/melee/constants.ts
+++ s/models/activity/reports/melee/constants.ts
@@ -5,7 +5,7 @@ export const canComboToSwing = true
 export const canComboFromStab = false
 export const canComboToStab = false
 
-export const comboableReleaseThreshold = 1 / 2
+export const comboableReleaseThreshold = 0
 export const minimumFeint = 0
 export const maximumFeint = 1
 
